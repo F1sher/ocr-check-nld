afc01f605c7a13da79e1d18a816f52bd6e99a1d1
dasalam <dasalam@gmail.com>
2019-01-22 21:23:36 +0300

    check is file exist?

--- ocr_check_nld/cli.py
+++ ocr_check_nld/cli.py
@@ -1,4 +1,4 @@
-from os.path import basename
+from os.path import basename, isfile
 import click
 from ocr_check_nld import Check
 
@@ -7,6 +7,10 @@ from ocr_check_nld import Check
 @click.argument("check_path")
 @click.argument("check_lang")
 def cli(check_path, check_lang):
+    if not isfile(check_path):
+        print("The file \"{:s}\" does not exist!".format(check_path))
+        return
+        
     check = Check(check_path, check_lang)
     total = check.get_total()
     print("Total of \"{:s}\" is {:.2f}".format(basename(check_path), total))
